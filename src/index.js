6431993a54fe5f7a021516ca8fcfb890fac4a84c
Roman Franko <roma.franko99@gmail.com>
2021-07-26 00:38:27 +0300

    Homework arrow function

--- src/index.js
+++ src/index.js
@@ -17,10 +17,11 @@ const reducer = (state = 0, action) => {
             return state = 0
         default:
             return state;
-    }
-}
-
+            }
+        }
+                
 const store = createStore(reducer);
+        
 
 mainNumber.textContent = 0;
 
@@ -28,22 +29,20 @@ store.subscribe(() => {
     mainNumber.textContent = store.getState();
 })
 
+const incf = () => {return {type: "INC"}}
+const decf = () => {return {type: "DEC"}}
+const updf = (value) => {return {type: "UPT"}}
+
+console.log(incf())
+
 inc.addEventListener('click', ()=>{
-    store.dispatch({type: "INC"});
+    store.dispatch(incf());
 });
 
 dec.addEventListener('click', ()=>{
-    store.dispatch({type: "DEC"});
+    store.dispatch(decf());
 });
 
 update.addEventListener('click', ()=>{
-    store.dispatch({type: "UPT"});
+    store.dispatch(updf());
 });
-
-
-
-
-
-store.subscribe(() => {
-    console.log(store.getState());
-})
