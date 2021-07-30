1668382860a0ee48970ddee649e2d3bd6846fc95
Roman Franko <roma.franko99@gmail.com>
2021-07-30 05:13:16 +0300

    Redux structure full

--- src/index.js
+++ src/index.js
@@ -1,54 +1,34 @@
 import { createStore, bindActionCreators } from "redux";
 import "./index.css"
 import reducer from "./reducer";
-import {incf, decf, updf, rndf} from "./actions";
+import * as actions from "./actions";
 
 const inc = document.querySelector('.inc');
 const dec = document.querySelector('.dec');
 const update = document.querySelector('.update');
 const random = document.querySelector('.random');
 const mainNumber = document.querySelector('.main-number');
-
-
   
 const store = createStore(reducer);
-        
 
-mainNumber.textContent = 0;
 
-store.subscribe(() => {
+const updatef = () => {
     mainNumber.textContent = store.getState();
-})
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//     dispatch(creator(...args));
-// }
+}
 
 
 const {dispatch} = store;
-// const incDispatch = () => dispatch(incf()); this function is refactored.
-const {incDispatch, decDispatch, updDispatch, rndDispatch} = bindActionCreators(
-    {
-        incDispatch: incf,
-        decDispatch: decf,
-        updDispatch: updf,
-        rndDispatch: rndf
-
-    }, dispatch);
-
-// const decDispatch = bindActionCreators(decf, dispatch);
-// const updDispatch = bindActionCreators(updf, dispatch);
-// const rndDispatch = bindActionCreators(rndf,dispatch);
+const {incf, decf, updf, rndf} = bindActionCreators(actions, dispatch);
 
 
-inc.addEventListener('click', ()=>{incDispatch()});
-
-dec.addEventListener('click', ()=>{decDispatch()});
-
-update.addEventListener('click', ()=>{updDispatch()});
-
+inc.addEventListener('click', incf);
+dec.addEventListener('click', decf);
+update.addEventListener('click', updf);
 random.addEventListener('click', ()=>{
     const value = Math.floor(Math.random()*10);
-    console.log(value)
-    rndDispatch(value)
+    rndf(value);
 });
+
+
+mainNumber.textContent = 0;
+store.subscribe(updatef);
